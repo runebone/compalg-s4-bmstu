021d052e9d25793144ae8151e2b2a0805a6f6f30
Konstantin Runov <runebone1@gmail.com>
2023-04-17 08:09:17 +0300

    add comments; remove unused numpy

--- lab3/interpolation.py
+++ lab3/interpolation.py
@@ -1,4 +1,3 @@
-import numpy as np
 from newton import interpolate_newton
 
 def interpolate_newton_2d(x, nx, x_list, table_1d):
@@ -10,6 +9,7 @@ def interpolate_newton_3d(x, y, nx, ny, x_list, y_list, table_2d):
     table[i] = array of function values
     table = array of arrays of function values
     """
+    # new_table_1d[i] = f(x, y_list[i])
     new_table_1d = []
     for i in range(len(y_list)):
         interp_result = interpolate_newton_2d(x, nx, x_list, table_2d[i])
@@ -24,6 +24,7 @@ def interpolate_newton_4d(x, y, z, nx, ny, nz, x_list, y_list, z_list, table_3d)
     table[A][B][C] = f(x_list[C], y_list[B], z_list[A])
     table[A][B] = array of f(x, y_list[B], z_list[A]) where x runs x_list values
     """
+    # new_table_1d[i] = f(x, y, z_list[i])
     new_table_1d = []
     for i in range(len(z_list)):
         interp_result = interpolate_newton_3d(x, y, nx, ny, x_list, y_list, table_3d[i])
